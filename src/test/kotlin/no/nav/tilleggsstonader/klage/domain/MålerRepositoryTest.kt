1a7651848d5c10b679b32b5c9fd6d744d3ff49c2
Viktor Solberg <viktor.grondalen.solberg@nav.no>
2024-06-05 13:55:43 +0200

    utbedret tester for MålerRepository

--- src/test/kotlin/no/nav/tilleggsstonader/klage/domain/MålerRepositoryTest.kt
+++ src/test/kotlin/no/nav/tilleggsstonader/klage/domain/MålerRepositoryTest.kt
@@ -24,36 +24,43 @@ internal class MålerRepositoryTest : IntegrationTest() {
 
     @BeforeEach
     internal fun setUp() {
-        val fagsak = fagsak()
-        testoppsettService.lagreFagsak(fagsak)
+        val fagsak1 = fagsak()
+        testoppsettService.lagreFagsak(fagsak1)
         testoppsettService.lagreBehandling(
             behandling(
-                fagsak,
+                fagsak1,
                 status = BehandlingStatus.FERDIGSTILT,
                 resultat = BehandlingResultat.MEDHOLD,
             ),
         )
         testoppsettService.lagreBehandling(
             behandling(
-                fagsak,
+                fagsak1,
                 status = BehandlingStatus.FERDIGSTILT,
                 resultat = BehandlingResultat.IKKE_MEDHOLD,
             ),
         )
         testoppsettService.lagreBehandling(
             behandling(
-                fagsak,
+                fagsak1,
                 status = BehandlingStatus.OPPRETTET,
                 resultat = BehandlingResultat.IKKE_SATT,
             ),
         )
 
-        val fagsakBarnetilsyn = fagsak(identer = setOf(PersonIdent("123")), stønadstype = Stønadstype.BARNETILSYN)
-        testoppsettService.lagreFagsak(fagsakBarnetilsyn)
+        val fagsak2 = fagsak(identer = setOf(PersonIdent("123")), stønadstype = Stønadstype.BARNETILSYN)
+        testoppsettService.lagreFagsak(fagsak2)
         testoppsettService.lagreBehandling(
             behandling(
-                fagsakBarnetilsyn,
+                fagsak2,
                 status = BehandlingStatus.FERDIGSTILT,
+                resultat = BehandlingResultat.MEDHOLD,
+            ),
+        )
+        testoppsettService.lagreBehandling(
+            behandling(
+                fagsak2,
+                status = BehandlingStatus.VENTER,
                 resultat = BehandlingResultat.MEDHOLD,
             ),
         )
@@ -64,9 +71,9 @@ internal class MålerRepositoryTest : IntegrationTest() {
         val data = målerRepository.finnBehandlingerPerStatus()
 
         assertThat(data).containsExactlyInAnyOrder(
-            BehandlingerPerStatus(Stønadstype.BARNETILSYN, BehandlingStatus.FERDIGSTILT, 2),
+            BehandlingerPerStatus(Stønadstype.BARNETILSYN, BehandlingStatus.FERDIGSTILT, 3),
             BehandlingerPerStatus(Stønadstype.BARNETILSYN, BehandlingStatus.OPPRETTET, 1),
-            BehandlingerPerStatus(Stønadstype.BARNETILSYN, BehandlingStatus.FERDIGSTILT, 1),
+            BehandlingerPerStatus(Stønadstype.BARNETILSYN, BehandlingStatus.VENTER, 1),
         )
     }
 
@@ -84,9 +91,8 @@ internal class MålerRepositoryTest : IntegrationTest() {
         val data = målerRepository.antallVedtak()
 
         assertThat(data).containsExactlyInAnyOrder(
-            AntallVedtak(Stønadstype.BARNETILSYN, BehandlingResultat.MEDHOLD, 1),
+            AntallVedtak(Stønadstype.BARNETILSYN, BehandlingResultat.MEDHOLD, 3),
             AntallVedtak(Stønadstype.BARNETILSYN, BehandlingResultat.IKKE_MEDHOLD, 1),
-            AntallVedtak(Stønadstype.BARNETILSYN, BehandlingResultat.MEDHOLD, 1),
         )
     }
 }
