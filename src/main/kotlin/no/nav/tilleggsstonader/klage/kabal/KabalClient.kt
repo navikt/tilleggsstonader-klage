099f8bad0e66d20045b313ab57f0020039cae101
Jan Eirik Nævdal <jan.eirik.b.navdal@nav.no>
2024-07-08 09:44:00 +0200

    Skifte ut bruk av AbstractRestClient fra familie til tilleggsstonader: KabalClient

--- src/main/kotlin/no/nav/tilleggsstonader/klage/kabal/KabalClient.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/klage/kabal/KabalClient.kt
@@ -1,12 +1,12 @@
 package no.nav.tilleggsstonader.klage.kabal
 
-import no.nav.familie.http.client.AbstractRestClient
+import no.nav.tilleggsstonader.libs.http.client.AbstractRestClient
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
-import org.springframework.web.client.RestOperations
+import org.springframework.web.client.RestTemplate
 import org.springframework.web.util.UriComponentsBuilder
 import java.net.URI
 
@@ -15,13 +15,13 @@ class KabalClient(
     @Value("\${KABAL_URL}")
     private val kabalUrl: URI,
     @Qualifier("azure")
-    private val restOperations: RestOperations,
-) : AbstractRestClient(restOperations, "tilleggsstonader.kabal") {
+    private val restTemplets: RestTemplate,
+) : AbstractRestClient(restTemplets) {
 
     val logger: Logger = LoggerFactory.getLogger(this::class.java)
 
-    private val oversendelseUrl: URI =
-        UriComponentsBuilder.fromUri(kabalUrl).pathSegment("api/oversendelse/v3/sak").build().toUri()
+    private val oversendelseUrl: String =
+        UriComponentsBuilder.fromUri(kabalUrl).pathSegment("api/oversendelse/v3/sak").build().toString()
 
     fun sendTilKabal(oversendtKlage: OversendtKlageAnkeV3) {
         return postForEntity(oversendelseUrl, oversendtKlage)
