b70e20ba8e2509d0bbc29c72c9df7a7bebb44240
Jan Eirik Nævdal <jan.eirik.b.navdal@nav.no>
2024-07-08 09:28:29 +0200

    Skifte ut bruk av AbstractRestClient fra familie til tilleggsstonader: SakClient

--- src/main/kotlin/no/nav/tilleggsstonader/klage/integrasjoner/TilleggsstonaderSakClient.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/klage/integrasjoner/TilleggsstonaderSakClient.kt
@@ -1,11 +1,9 @@
 package no.nav.tilleggsstonader.klage.integrasjoner
 
-import no.nav.familie.http.client.AbstractRestClient
-import no.nav.tilleggsstonader.klage.Ressurs
+import no.nav.tilleggsstonader.libs.http.client.AbstractRestClient
 import no.nav.tilleggsstonader.klage.felles.dto.EgenAnsattRequest
 import no.nav.tilleggsstonader.klage.felles.dto.EgenAnsattResponse
 import no.nav.tilleggsstonader.klage.felles.dto.Tilgang
-import no.nav.tilleggsstonader.klage.getDataOrThrow
 import no.nav.tilleggsstonader.kontrakter.felles.IdentRequest
 import no.nav.tilleggsstonader.kontrakter.klage.FagsystemVedtak
 import no.nav.tilleggsstonader.kontrakter.klage.KanOppretteRevurderingResponse
@@ -13,29 +11,29 @@ import no.nav.tilleggsstonader.kontrakter.klage.OpprettRevurderingResponse
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
-import org.springframework.web.client.RestOperations
+import org.springframework.web.client.RestTemplate
 import java.net.URI
 
 @Component
 class TilleggsstonaderSakClient(
-    @Qualifier("azure") restOperations: RestOperations,
+    @Qualifier("azure") restTemplate: RestTemplate,
     @Value("\${TILLEGGSSTONADER_SAK_URL}") private val sakUrl: String,
-) : AbstractRestClient(restOperations, "tilleggsstonader.sak") {
+) : AbstractRestClient(restTemplate) {
 
     fun hentVedtak(fagsystemEksternFagsakId: String): List<FagsystemVedtak> {
-        val hentVedtakUri = URI.create("$sakUrl/api/klage/ekstern-fagsak/$fagsystemEksternFagsakId/vedtak")
-        return getForEntity<Ressurs<List<FagsystemVedtak>>>(hentVedtakUri).getDataOrThrow()
+        val hentVedtakUri = URI.create("$sakUrl/api/klage/ekstern-fagsak/$fagsystemEksternFagsakId/vedtak").toString()
+        return getForEntity<List<FagsystemVedtak>>(hentVedtakUri)
     }
 
     fun kanOppretteRevurdering(fagsystemEksternFagsakId: String): KanOppretteRevurderingResponse {
         val hentVedtakUri =
-            URI.create("$sakUrl/api/ekstern/behandling/kan-opprette-revurdering-klage/$fagsystemEksternFagsakId")
+            URI.create("$sakUrl/api/ekstern/behandling/kan-opprette-revurdering-klage/$fagsystemEksternFagsakId").toString()
         return getForEntity<KanOppretteRevurderingResponse>(hentVedtakUri)
     }
 
     fun opprettRevurdering(fagsystemEksternFagsakId: String): OpprettRevurderingResponse {
         val hentVedtakUri =
-            URI.create("$sakUrl/api/ekstern/behandling/opprett-revurdering-klage/$fagsystemEksternFagsakId")
+            URI.create("$sakUrl/api/ekstern/behandling/opprett-revurdering-klage/$fagsystemEksternFagsakId").toString()
         return postForEntity<OpprettRevurderingResponse>(
             hentVedtakUri,
             emptyMap<String, String>()
@@ -44,14 +42,14 @@ class TilleggsstonaderSakClient(
 
     fun sjekkTilgangTilPersonMedRelasjoner(ident: String): Tilgang {
         return postForEntity<Tilgang>(
-            URI.create("$sakUrl/api/tilgang/person/sjekkTilgangTilPersonMedRelasjoner"),
+            URI.create("$sakUrl/api/tilgang/person/sjekkTilgangTilPersonMedRelasjoner").toString(),
             IdentRequest(ident = ident)
         )
     }
 
     fun erEgenAnsatt(ident: String): Boolean {
         return postForEntity<EgenAnsattResponse>(
-            URI.create("$sakUrl/api/tilgang/person/erEgenAnsatt"),
+            URI.create("$sakUrl/api/tilgang/person/erEgenAnsatt").toString(),
             EgenAnsattRequest(ident),
         ).erEgenAnsatt
     }
