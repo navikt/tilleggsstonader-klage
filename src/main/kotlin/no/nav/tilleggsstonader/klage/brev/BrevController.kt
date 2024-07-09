a66fc3cad3856108939f701d226ae36dc4905e2d
Runar Skagestad <runar.skagestad@nav.no>
2024-07-09 16:10:21 +0200

    Bytt til base64-encoding av brev og fjern Ressurs-wrap

--- src/main/kotlin/no/nav/tilleggsstonader/klage/brev/BrevController.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/klage/brev/BrevController.kt
@@ -6,6 +6,7 @@ import no.nav.tilleggsstonader.klage.brev.dto.BrevmottakereDto
 import no.nav.tilleggsstonader.klage.brev.dto.tilDto
 import no.nav.tilleggsstonader.klage.felles.domain.AuditLoggerEvent
 import no.nav.tilleggsstonader.klage.infrastruktur.sikkerhet.TilgangService
+import org.slf4j.LoggerFactory
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.Base64
 import java.util.UUID
 
 @RestController
@@ -23,19 +25,18 @@ class BrevController(
     private val brevService: BrevService,
     private val tilgangService: TilgangService,
 ) {
-
     @GetMapping("/{behandlingId}/pdf")
-    fun hentBrevPdf(@PathVariable behandlingId: UUID): Ressurs<ByteArray> {
+    fun hentBrevPdf(@PathVariable behandlingId: UUID): ByteArray {
         tilgangService.validerTilgangTilPersonMedRelasjonerForBehandling(behandlingId, AuditLoggerEvent.ACCESS)
         tilgangService.validerHarVeilederrolleTilStønadForBehandling(behandlingId)
-        return Ressurs.success(brevService.hentBrevPdf(behandlingId))
+        return Base64.getEncoder().encode(brevService.hentBrevPdf(behandlingId))
     }
 
     @PostMapping("/{behandlingId}")
-    fun lagEllerOppdaterBrev(@PathVariable behandlingId: UUID): Ressurs<ByteArray> {
+    fun lagEllerOppdaterBrev(@PathVariable behandlingId: UUID): ByteArray {
         tilgangService.validerTilgangTilPersonMedRelasjonerForBehandling(behandlingId, AuditLoggerEvent.UPDATE)
         tilgangService.validerHarSaksbehandlerrolleTilStønadForBehandling(behandlingId)
-        return Ressurs.success(brevService.lagBrev(behandlingId))
+        return Base64.getEncoder().encode(brevService.lagBrev(behandlingId))
     }
 
     @GetMapping("/{behandlingId}/mottakere")
