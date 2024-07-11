11396a0a3011b7b2d7be431dd4913374142b3d70
Runar Skagestad <runar.skagestad@nav.no>
2024-07-11 10:57:17 +0200

    Gjør ApiExceptionHandler RFC 7807-kompatibel

--- src/main/kotlin/no/nav/tilleggsstonader/klage/infrastruktur/exception/ApiExceptionHandler.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/klage/infrastruktur/exception/ApiExceptionHandler.kt
@@ -1,109 +1,121 @@
 package no.nav.tilleggsstonader.klage.infrastruktur.exception
 
 import no.nav.security.token.support.core.exceptions.JwtTokenMissingException
-import no.nav.tilleggsstonader.klage.Ressurs
+import no.nav.tilleggsstonader.libs.log.SecureLogger
 import org.slf4j.LoggerFactory
 import org.springframework.core.NestedExceptionUtils
 import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
+import org.springframework.http.ProblemDetail
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseStatus
+import java.net.SocketTimeoutException
+import java.util.concurrent.TimeoutException
 
-@Suppress("unused")
 @ControllerAdvice
 class ApiExceptionHandler {
 
-    private val logger = LoggerFactory.getLogger(ApiExceptionHandler::class.java)
-    private val secureLogger = LoggerFactory.getLogger("secureLogger")
-
-    private fun rootCause(throwable: Throwable): String {
-        return NestedExceptionUtils.getMostSpecificCause(throwable).javaClass.simpleName
-    }
+    private val logger = LoggerFactory.getLogger(javaClass)
+    private val secureLogger = SecureLogger.secureLogger
 
     @ExceptionHandler(Throwable::class)
-    fun handleThrowable(throwable: Throwable): ResponseEntity<Ressurs<Nothing>> {
+    fun handleThrowable(throwable: Throwable): ProblemDetail {
+        val responseStatus = throwable::class.annotations.find { it is ResponseStatus }
+            ?.let { it as ResponseStatus }
+            ?.value
+            ?: HttpStatus.INTERNAL_SERVER_ERROR
+
         val metodeSomFeiler = finnMetodeSomFeiler(throwable)
+
+        val mostSpecificCause = throwable.getMostSpecificCause()
+        if (mostSpecificCause is SocketTimeoutException || mostSpecificCause is TimeoutException) {
+            secureLogger.warn(
+                "Timeout feil: ${mostSpecificCause.message}, $metodeSomFeiler ${rootCause(throwable)}",
+                throwable,
+            )
+            logger.warn("Timeout feil: $metodeSomFeiler ${rootCause(throwable)} ")
+            return lagTimeoutfeilRessurs()
+        }
+
         secureLogger.error("Uventet feil: $metodeSomFeiler ${rootCause(throwable)}", throwable)
         logger.error("Uventet feil: $metodeSomFeiler ${rootCause(throwable)} ")
 
-        return ResponseEntity
-            .status(HttpStatus.INTERNAL_SERVER_ERROR)
-            .body(Ressurs.failure(errorMessage = "Uventet feil", frontendFeilmelding = "En uventet feil oppstod."))
+        return ProblemDetail.forStatusAndDetail(responseStatus, "Ukjent feil")
     }
 
     @ExceptionHandler(JwtTokenMissingException::class)
-    fun handleJwtTokenMissingException(jwtTokenMissingException: JwtTokenMissingException): ResponseEntity<Ressurs<Nothing>> {
-        return ResponseEntity
-            .status(HttpStatus.UNAUTHORIZED)
-            .body(
-                Ressurs.failure(
-                    errorMessage = "401 Unauthorized JwtTokenMissingException",
-                    frontendFeilmelding = "En uventet feil oppstod: Kall ikke autorisert",
-                ),
-            )
-    }
-
-    @ExceptionHandler(ApiFeil::class)
-    fun handleThrowable(feil: ApiFeil): ResponseEntity<Ressurs<Nothing>> {
-        val metodeSomFeiler = finnMetodeSomFeiler(feil)
-        secureLogger.info("En håndtert feil har oppstått(${feil.httpStatus}): ${feil.feil}", feil)
-        logger.info("En håndtert feil har oppstått(${feil.httpStatus}) metode=$metodeSomFeiler exception=${rootCause(feil)}: ${feil.message} ")
-        return ResponseEntity.status(feil.httpStatus).body(
-            Ressurs.funksjonellFeil(
-                frontendFeilmelding = feil.feil,
-                melding = feil.feil,
-            ),
+    fun handleJwtTokenMissingException(jwtTokenMissingException: JwtTokenMissingException): ProblemDetail {
+        return ProblemDetail.forStatusAndDetail(
+            HttpStatus.UNAUTHORIZED,
+            "En uventet feil oppstod: Kall ikke autorisert",
         )
     }
 
+    @ExceptionHandler(ApiFeil::class)
+    fun handleThrowable(feil: ApiFeil): ProblemDetail {
+        val metodeSomFeiler = finnMetodeSomFeiler(feil)
+        secureLogger.info("En håndtert feil har oppstått(${feil.httpStatus}): ${feil.feil}", feil)
+        logger.info(
+            "En håndtert feil har oppstått(${feil.httpStatus}) " +
+                    "metode=$metodeSomFeiler exception=${rootCause(feil)}: ${feil.message} ",
+        )
+        return ProblemDetail.forStatusAndDetail(feil.httpStatus, feil.feil)
+    }
+
     @ExceptionHandler(Feil::class)
-    fun handleThrowable(feil: Feil): ResponseEntity<Ressurs<Nothing>> {
+    fun handleThrowable(feil: Feil): ProblemDetail {
         val metodeSomFeiler = finnMetodeSomFeiler(feil)
         secureLogger.error("En håndtert feil har oppstått(${feil.httpStatus}): ${feil.frontendFeilmelding}", feil)
-        logger.error("En håndtert feil har oppstått(${feil.httpStatus}) metode=$metodeSomFeiler exception=${rootCause(feil)}: ${feil.message} ")
-        return ResponseEntity.status(feil.httpStatus).body(Ressurs.failure(frontendFeilmelding = feil.frontendFeilmelding))
+        logger.error(
+            "En håndtert feil har oppstått(${feil.httpStatus}) " +
+                    "metode=$metodeSomFeiler exception=${rootCause(feil)}: ${feil.message} ",
+        )
+        return ProblemDetail.forStatusAndDetail(feil.httpStatus, feil.frontendFeilmelding)
     }
 
     @ExceptionHandler(PdlNotFoundException::class)
-    fun handleThrowable(feil: PdlNotFoundException): ResponseEntity<Ressurs<Nothing>> {
+    fun handleThrowable(feil: PdlNotFoundException): ProblemDetail {
         logger.warn("Finner ikke personen i PDL")
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Ressurs.failure(frontendFeilmelding = "Finner ikke personen"))
+        return ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, "Finner ingen personer for valgt personident")
     }
 
     @ExceptionHandler(ManglerTilgang::class)
-    fun handleThrowable(manglerTilgang: ManglerTilgang): ResponseEntity<Ressurs<Nothing>> {
+    fun handleThrowable(manglerTilgang: ManglerTilgang): ProblemDetail {
         secureLogger.warn("En håndtert tilgangsfeil har oppstått - ${manglerTilgang.melding}", manglerTilgang)
         logger.warn("En håndtert tilgangsfeil har oppstått")
-        return ResponseEntity.status(HttpStatus.FORBIDDEN)
-            .body(
-                Ressurs(
-                    data = null,
-                    status = Ressurs.Status.IKKE_TILGANG,
-                    frontendFeilmelding = manglerTilgang.frontendFeilmelding,
-                    melding = manglerTilgang.melding,
-                    stacktrace = null,
-                ),
-            )
+        return ProblemDetail.forStatusAndDetail(HttpStatus.FORBIDDEN, manglerTilgang.frontendFeilmelding)
     }
 
     @ExceptionHandler(IntegrasjonException::class)
-    fun handleThrowable(feil: IntegrasjonException): ResponseEntity<Ressurs<Nothing>> {
+    fun handleThrowable(feil: IntegrasjonException): ProblemDetail {
         secureLogger.error("Feil mot integrasjonsclienten har oppstått: uri={} data={}", feil.uri, feil.data, feil)
         logger.error("Feil mot integrasjonsclienten har oppstått exception=${rootCause(feil)}")
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-            .body(Ressurs.failure(frontendFeilmelding = feil.message))
+        return ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, feil.message ?: "Mangler melding")
     }
+
+    private fun lagTimeoutfeilRessurs(): ProblemDetail = ProblemDetail.forStatusAndDetail(
+        HttpStatus.INTERNAL_SERVER_ERROR,
+        "Kommunikasjonsproblemer med andre systemer - prøv igjen",
+    )
 
     fun finnMetodeSomFeiler(e: Throwable): String {
         val firstElement = e.stackTrace.firstOrNull {
             it.className.startsWith("no.nav.tilleggsstonader.klage") &&
-                !it.className.contains("$") &&
-                !it.className.contains("InsertUpdateRepositoryImpl")
+                    !it.className.contains("$") &&
+                    !it.className.contains("InsertUpdateRepositoryImpl")
         }
         if (firstElement != null) {
             val className = firstElement.className.split(".").lastOrNull()
             return "$className::${firstElement.methodName}(${firstElement.lineNumber})"
         }
         return e.cause?.let { finnMetodeSomFeiler(it) } ?: "(Ukjent metode som feiler)"
+    }
+
+    private fun rootCause(throwable: Throwable): String {
+        return throwable.getMostSpecificCause().javaClass.simpleName
+    }
+
+    private fun Throwable.getMostSpecificCause(): Throwable {
+        return NestedExceptionUtils.getMostSpecificCause(this)
     }
 }
