b1628840507bfba47e10b2df997449f761850d13
eirikvilpponen <eirik.vilpponen@nav.no>
2024-07-11 12:34:07 +0200

    Fjerne endepunkt for endepunkt for søk på person da dette ikke er i bruk lenger. Søk gjøres mot TS-SAK

--- src/main/kotlin/no/nav/tilleggsstonader/klage/søk/dto/SøkDto.kt
+++
@@ -1,7 +0,0 @@
-package no.nav.tilleggsstonader.klage.søk.dto
-
-import java.util.UUID
-
-data class PersonIdentDto(val personIdent: String, val behandlingId: UUID)
-
-data class PersonTreffDto(val personIdent: String, val navn: String)
