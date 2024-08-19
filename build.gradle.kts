2b3ce37e5c57e2ff8883eb32cafc486c5ecfe48c
Runar Skagestad <runar.skagestad@nav.no>
2024-08-19 11:09:35 +0200

    Legg inn cyclonedx.bom som gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,8 @@ plugins {
     id("io.spring.dependency-management") version "1.1.6"
 
     kotlin("plugin.spring") version "1.9.24"
+
+    id("org.cyclonedx.bom") version "1.8.2"
 }
 
 repositories {
