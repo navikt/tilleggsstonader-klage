ba07a135633c44447bf3c4164dd23980cd0bb845
Johan Blomgren <blommish@users.noreply.github.com>
2025-02-27 12:15:18 +0100

    RetryOAuth2HttpClient er allerede definiert i RestTemplateConfiguration

--- src/main/kotlin/no/nav/tilleggsstonader/klage/infrastruktur/config/ApplicationConfig.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/klage/infrastruktur/config/ApplicationConfig.kt
@@ -1,29 +1,21 @@
 package no.nav.tilleggsstonader.klage.infrastruktur.config
 
 import no.nav.familie.prosessering.config.ProsesseringInfoProvider
-import no.nav.security.token.support.client.core.http.OAuth2HttpClient
-import no.nav.security.token.support.client.core.oauth2.OAuth2AccessTokenResponse
 import no.nav.security.token.support.client.spring.oauth2.EnableOAuth2Client
 import no.nav.security.token.support.spring.SpringTokenValidationContextHolder
 import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
 import no.nav.tilleggsstonader.klage.infrastruktur.filter.NAVIdentFilter
 import no.nav.tilleggsstonader.klage.infrastruktur.sikkerhet.SikkerhetContext
-import no.nav.tilleggsstonader.libs.http.client.RetryOAuth2HttpClient
 import no.nav.tilleggsstonader.libs.http.config.RestTemplateConfiguration
 import no.nav.tilleggsstonader.libs.log.filter.LogFilterConfiguration
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.SpringBootConfiguration
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
-import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.boot.web.servlet.FilterRegistrationBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Import
-import org.springframework.context.annotation.Primary
 import org.springframework.scheduling.annotation.EnableScheduling
-import org.springframework.web.client.RestClient
-import java.time.Duration
-import java.time.temporal.ChronoUnit
 
 @SpringBootConfiguration
 @ConfigurationPropertiesScan
@@ -33,22 +25,6 @@ import java.time.temporal.ChronoUnit
 @EnableOAuth2Client(cacheEnabled = true)
 @EnableScheduling
 class ApplicationConfig {
-    /**
-     * Overskrever OAuth2HttpClient som settes opp i token-support som ikke kan få med objectMapper fra felles
-     * pga .setVisibility(PropertyAccessor.SETTER, JsonAutoDetect.Visibility.NONE)
-     * og [OAuth2AccessTokenResponse] som burde settes med setters, då feltnavn heter noe annet enn feltet i json
-     */
-    @Bean
-    @Primary
-    fun oAuth2HttpClient(): OAuth2HttpClient =
-        RetryOAuth2HttpClient(
-            RestClient.create(
-                RestTemplateBuilder()
-                    .setConnectTimeout(Duration.of(2, ChronoUnit.SECONDS))
-                    .setReadTimeout(Duration.of(2, ChronoUnit.SECONDS))
-                    .build(),
-            ),
-        )
 
     @Bean
     fun prosesseringInfoProvider(
