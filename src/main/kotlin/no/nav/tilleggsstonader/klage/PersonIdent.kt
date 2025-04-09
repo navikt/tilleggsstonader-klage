a540033c0646d4966b7e2f977318ba030f2691ae
Runar Skagestad <runar.skagestad@nav.no>
2025-04-09 18:21:52 +0200

    🔥 Fjern ubrukt klasse PersonIdent

--- src/main/kotlin/no/nav/tilleggsstonader/klage/PersonIdent.kt
+++
@@ -1,7 +0,0 @@
-package no.nav.tilleggsstonader.klage
-
-import jakarta.validation.constraints.Pattern
-
-data class PersonIdent(
-    @field:Pattern(regexp = "(^$|.{11})", message = "PersonIdent er ikke riktig") val ident: String,
-)
