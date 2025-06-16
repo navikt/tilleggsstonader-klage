f39b511f378258bfe13f4e1339ba4053708e711b
Johan Blomgren <blommish@users.noreply.github.com>
2025-06-16 09:15:09 +0200

    Oppdaterer kontrakter og ruller tilbake spring boot for å vente litt før vi oppdaterer den

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ plugins {
     id("com.github.ben-manes.versions") version "0.52.0"
     id("se.patrikerdes.use-latest-versions") version "0.2.18"
 
-    id("org.springframework.boot") version "3.5.0"
+    id("org.springframework.boot") version "3.4.5"
     id("io.spring.dependency-management") version "1.1.7"
 
     id("org.cyclonedx.bom") version "2.3.1"
