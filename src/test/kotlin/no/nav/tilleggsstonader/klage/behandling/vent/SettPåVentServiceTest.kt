2f29fa906113198051afdb26f499cb3dfe238645
Andreas Strand <andreas.strand@nav.no>
2026-03-12 13:24:36 +0100

    Legger til .editorconfig og kjører spotlessApply som fjerner ubrukte imports

--- src/test/kotlin/no/nav/tilleggsstonader/klage/behandling/vent/SettPåVentServiceTest.kt
+++ src/test/kotlin/no/nav/tilleggsstonader/klage/behandling/vent/SettPåVentServiceTest.kt
@@ -28,7 +28,6 @@ import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import java.time.LocalDate
-import java.time.LocalDateTime
 import java.util.Optional
 import kotlin.jvm.optionals.getOrNull
 
